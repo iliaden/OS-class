e40bdf5fd60dd9bc515b50e53eabbc2c838c1c8c
Ilia Denotkine <denotkine@gmail.com>
2012-02-21 00:40:21 -0500

    works, bitches. remaining: -v levels. stop condition, maybe

--- ass2/os.c
+++ ass2/os.c
@@ -4,6 +4,7 @@
 #include <sys/wait.h>
 #include <errno.h>
 #include <pthread.h>
+#include <unistd.h>
 
 #define WORKING 1
 #define WAITING 2
@@ -11,12 +12,24 @@
 #define UP 0
 #define DOWN 1
 
-pthread_mutex_t clock_mutex = PTHREAD_MUTEX_INITIALIZER; 
+#define MAXDEBUG 0
+#define AVGDEBUG 1
+#define AGGREGATE 2
+
 pthread_mutex_t in_mutex = PTHREAD_MUTEX_INITIALIZER; 
 pthread_mutex_t out_mutex = PTHREAD_MUTEX_INITIALIZER; 
 pthread_mutex_t in_ding_mutex = PTHREAD_MUTEX_INITIALIZER; 
 pthread_mutex_t out_ding_mutex = PTHREAD_MUTEX_INITIALIZER; 
 pthread_mutex_t elevator_floor_mutex = PTHREAD_MUTEX_INITIALIZER; 
+
+pthread_cond_t in_ding_waiter = PTHREAD_COND_INITIALIZER;
+pthread_cond_t out_ding_waiter = PTHREAD_COND_INITIALIZER;
+
+//clock
+pthread_mutex_t clock_mutex = PTHREAD_MUTEX_INITIALIZER; 
+pthread_cond_t tick_waiter = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t tick_mutex = PTHREAD_MUTEX_INITIALIZER; 
+
 
 
 int time_elapsed; //main() initializes to 0. everyone can read. clock can write.
@@ -25,19 +38,23 @@ int **requests; //main() must allocate the memory - [2][floor_count]
 int *targets;   // main() must allocate the memory - [floor_count]
 int passenger_count=0;
 int elevator_floor;
+int debugLvl = MAXDEBUG;
+int max_work_time;
+
+void *my_clock(void* interval); //main ticker
+void *client(void* work_len); //client thread. one for each client
+void *elevator(void* max_cap); //elevator thread
 
 int my_random(int mod); //gets a random number between 0 and mod
 int read_time(); //protected call to know the time elapsed since start of simulation
-int my_clock(int interval); //main ticker
 void wait_for_tick(); //waits until the next clock tick.
-int client(int work_len); //client thread. one for each client
-int elevator(int max_cap); //elevator thread
 void send_out_ding(); //lets one person out. blockingg call.
-void stop_out_ding(); //lets one person out. blockingg call.
-void stop_in_ding(); //lets one person out. blockingg call.
 void send_in_ding(); //let one person in. blocks until mutex is released by other thread.
 void wait_for_out_ding(); //blocking call
 void wait_for_in_ding();  //blocking call
+void press_button(int curr_floor, int target_floor, int client_id); //press the right button (up/down)
+int get_inside(int curr_floor, int target_floor); //allows one person to enter elevator
+int get_outside(int floor); //allows one person to leave the elevator
 
 int get_elevator_floor()
 {
@@ -45,57 +62,54 @@ int get_elevator_floor()
     pthread_mutex_lock( &elevator_floor_mutex );
     tmp = elevator_floor;
     pthread_mutex_unlock( &elevator_floor_mutex );
-    return time_elapsed;
+    return tmp;
 }
 
 int my_random(int mod)
 {
-    srand (time(NULL)); //should be done once... to be fixed eventually
     return rand() % mod;
 }
 
 //TIME/clock - related
 int read_time()
 {
-    int tmp;
+/*    int tmp;
     pthread_mutex_lock( &clock_mutex );
     tmp = time_elapsed;
     pthread_mutex_unlock( &clock_mutex );
-    return time_elapsed;
+*/    return time_elapsed;
 }
-int my_clock(int interval)
+void * my_clock(void * arg)
 {
+    int interval = (int)arg;
     while(1)
     {
         //increment time_elapsed
-        pthread_mutex_lock( &clock_mutex );
         time_elapsed++;
-        pthread_mutex_unlock( &clock_mutex );
 
+        printf("\n\nStarting tick %d\n", time_elapsed);
         //send tick.
-        
+        pthread_cond_broadcast( &tick_waiter);        
         //wait 1 clock cycle
         usleep(interval);
     }
 }
 void wait_for_tick() //waits until the next clock tick.
 {
-    int start_tick = read_time();
-    while(1)
-    {
-        if ( read_time() > start_tick)
-            return;
-    }
+    pthread_cond_wait( &tick_waiter, &tick_mutex);
+    pthread_mutex_unlock(&tick_mutex);
 }
 
 //CLIENT - related
 
-int client(int work_len)
+void *client(void * arg )
 {
+    int work_len = 1+my_random(max_work_time-1);
+    int worker_id = (int)arg;
     //init
     int state = WORKING;
     int worked_time = my_random(work_len); 
-    int target_floor = 0;
+    int target_floor = my_random(floor_count);
     int curr_floor = my_random(floor_count); // we start at a random floor
 
     //main loop
@@ -111,19 +125,21 @@ int client(int work_len)
             {  
                 while ( target_floor == curr_floor)
                     target_floor = my_random(floor_count); //avoids going to current floor
-                press_button(target_floor, curr_floor);
+                press_button(curr_floor, target_floor,worker_id);
                 state = WAITING;                
             }
         }
         else if ( state == WAITING)
         {
             wait_for_in_ding(); // a single in ding happens at each clock cycle
+            
             if ( get_elevator_floor() == curr_floor )
             {
                 //get inside...quick!
                 //or at least try
                 if (get_inside(curr_floor, target_floor) == 0 ) //operation succeeded. and that is a blocking operation through mutex
                 {
+                    printf("Client %d enetered the elevator at floor %d heading toward floor %d\n",worker_id,curr_floor,target_floor);
                     state = INTRANSIT;
                 }
             }
@@ -135,7 +151,10 @@ int client(int work_len)
             if (curr_floor == target_floor)
             {
                 get_outside(target_floor);
+                printf("Client %d left the elevator at floor %d and will now word for %d ticks.\n",worker_id,curr_floor,work_len);
                 state=WORKING;
+                worked_time = 0;
+                work_len = 1+my_random(max_work_time-1);
             }
         }
         else
@@ -144,22 +163,25 @@ int client(int work_len)
         }
     }
 
+    return NULL;
 }
 
-int press_button(curr_floor, target_floor)
+void press_button(int curr_floor, int target_floor, int client_id)
 {
     if (curr_floor < target_floor) //FIXME: protected code
     {
         requests[UP][curr_floor]++;
+        printf("Client %d pressed button UP at floor %d, hoping to get to floor %d.\n",client_id,curr_floor,target_floor);
     }
     else
     {
         requests[DOWN][curr_floor]++;
+        printf("Client %d pressed button DOWN at floor %d, hoping to get to floor %d.\n",client_id,curr_floor,target_floor);
     }
     //release semaphore
 }
 
-int get_inside(curr_floor, target_floor) 
+int get_inside(int curr_floor, int target_floor) 
 {
     if (curr_floor < target_floor) //FIXME: protected code
     {
@@ -172,6 +194,7 @@ int get_inside(curr_floor, target_floor)
     targets[target_floor]++;
     passenger_count++;
     //FIXME: release semaphore
+    return 0;
 }
 
 
@@ -187,6 +210,10 @@ int get_outside(int floor) //allows one person to leave the elevator
 int compute_direction(int dir, int curr_floor)
 {
     int ii;
+    if (curr_floor == 0)
+        return UP;
+    if (curr_floor == floor_count)
+        return DOWN;
     if (dir == UP)
     {
         //we make sure we're not at the top floor
@@ -194,13 +221,13 @@ int compute_direction(int dir, int curr_floor)
             return DOWN;
         //we check that someone INSIDE the elevator wants to get higher
         for ( ii=curr_floor+1 ; ii < floor_count; ii++)
-        {//FIXME: semaphore here?
+        {// semaphore here?
             if (targets[ii] > 0 )
                 return UP;
         }
         //we check that there are requests from above
         for ( ii=curr_floor+1 ; ii < floor_count; ii++)
-        {//FIXME: semaphore here?
+        {//semaphore here?
             if (requests[UP][ii] > 0 )
                 return UP;
             else if (requests[DOWN][ii] > 0)
@@ -216,13 +243,13 @@ int compute_direction(int dir, int curr_floor)
             return UP;
         //we check that someone INSIDE the elevator wants to get lower
         for ( ii=curr_floor-1 ; ii >= 0 ; ii--)
-        {//FIXME: semaphore here?
+        {//semaphore here?
             if (targets[ii] > 0 )
                 return DOWN;
         }
         //we check that there are requests from above
         for ( ii=curr_floor-1 ; ii >= 0 ; ii--)
-        {//FIXME: semaphore here?
+        {//semaphore here?
             if (requests[UP][ii] > 0 )
                 return DOWN;
             else if (requests[DOWN][ii] > 0)
@@ -231,28 +258,29 @@ int compute_direction(int dir, int curr_floor)
         return UP;
         //option to add the idea of not moving if there are no requests...
     }
-
+    return -1;
 }
 
-int elevator(int max_cap)
+void *elevator(void * arg)
 {
+    int max_capacity = (int)arg;
     //init?
     int dir = UP;    
     int curr_floor = 0;
-    int max_capacity=max_cap;
 
     while(1)
     {
+        
         wait_for_tick(); //everything starts with a tick from the clock
         dir = compute_direction(dir, curr_floor); //where we need to go, given curr dir & floor
         if ( dir == UP)
         {
             //we move up
-            curr_floor++;
+            printf("elevator moved UP to floor %d\n", ++curr_floor);
         }
         else if (dir == DOWN)
         {   //am I forgetting something here?
-            curr_floor--;
+            printf("elevator moved DOWN to floor %d\n", --curr_floor);
         }
         else 
         {
@@ -268,97 +296,152 @@ int elevator(int max_cap)
         while (targets[curr_floor] > 0 )
         {
             send_out_ding();
-            //wait until we know someone got the ding...
-            stop_out_ding();
         }
 
         //let passengers in
-        while ( requests[dir/*0=up*/][curr_floor] > 0 && passenger_count < max_capacity )
+        while ( ((requests[dir/*0=up*/][curr_floor] > 0) || ((requests[1-dir][0] > 0) && curr_floor==0) || ((requests[1-dir][floor_count] > 0) && curr_floor==floor_count) ) && passenger_count < max_capacity )
         {
             send_in_ding();            
         }
-
     }
-
 }
-
-int out_ding = 0;
-int out_ding_ack = 0;
 
 void send_out_ding() //lets one person out. blocking call.
 {
-    out_ding=1;
-    out_ding_ack=0;
-    while( out_ding_ack == 0)
-        ; //we wait
-    return;
-}
-void stop_out_ding()
-{
-    out_ding=0;
+    pthread_cond_signal(&out_ding_waiter);
 }
 void wait_for_out_ding() //blocking call
 {
-    while ( out_ding != 1 && out_ding_ack != 0 )
-    {
-        ;// we wait for an out_ding to be available, and nobody else has taken it
-    }
-    // a ding has occurred
-    
-    //FIXME: synchronize 
-    int waiting=0;
-    pthread_mutex_lock( &out_ding_mutex );
-    if (out_ding_ack != 1 ) //if free, we take it
-        out_ding_ack=1;
-    else 
-    {   waiting =1;
-    }
-    pthread_mutex_unlock( &out_ding_mutex );
-    if ( waiting)
-        wait_for_out_ding(); //otherwise, we wait some more
-    return;
+    pthread_mutex_lock(&out_ding_mutex);    
+    pthread_cond_wait(&out_ding_waiter, &out_ding_mutex);
+    pthread_mutex_unlock(&out_ding_mutex);
 }
-
-int in_ding = 0;
-int in_ding_ack = 0;
-
 void send_in_ding() //lets one person in. blocking call.
 {
-    in_ding=1;
-    in_ding_ack=0;
-    while( in_ding_ack == 0)
-        ; //we wait
-    return;
-}
-void stop_in_ding()
-{
-    in_ding=0;
+    pthread_mutex_lock(&in_ding_mutex);    
+    pthread_cond_signal(&in_ding_waiter);
+    pthread_mutex_unlock(&in_ding_mutex);
 }
 void wait_for_in_ding() //blocking call
 {
-    while ( in_ding != 1 && in_ding_ack != 0 )
-    {
-        ;// we wait for an out_ding to be available, and nobody else has taken it
-    }
-    // a ding has occurred
-    
-    //FIXME: synchronize 
-    int waiting=0;
-    pthread_mutex_lock( &in_ding_mutex );
-    if (in_ding_ack != 1 ) //if free, we take it
-        in_ding_ack=1;
-    else 
-    {   waiting =1;
-    }
-    pthread_mutex_unlock( &in_ding_mutex );
-    if ( waiting)
-        wait_for_in_ding(); //otherwise, we wait some more
-    return;
+    pthread_mutex_lock(&in_ding_mutex);    
+    pthread_cond_wait(&in_ding_waiter, &in_ding_mutex);
+    pthread_mutex_unlock(&in_ding_mutex);
 }
 
-
-int main()
+int main(int argc, char * argv[])
 {
     //init
+    srand (time(NULL)); //should be done once... to be fixed eventually
     time_elapsed=0;
+    int clients_num=50;
+    int max_capacity = 2147483647;
+    int tick_interval = 100000;
+    max_work_time=30;
+    int ii; 
+    floor_count=50;
+    elevator_floor=0;
+
+    //parse input
+    //
+    //format:   -n number of floors
+    //          -p number of people
+    //          -m max_capacity
+    //          -w max_work_len
+    //          -t number of miliseconds per tick
+    //          -vvv  //max details (everything)
+    //          -vv //medium: who is entering/exiting
+    //          -v //100-ticks aggregations
+    for ( ii = 1; ii < argc; ii++)
+    {
+        if ( strcmp(argv[ii], "-v" ) == 0 )
+            debugLvl = AGGREGATE;    
+        else if ( strcmp(argv[ii], "-vv" ) == 0 )
+            debugLvl = AVGDEBUG;    
+        else if ( strcmp(argv[ii], "-vvv" ) == 0 )
+            debugLvl = MAXDEBUG;    
+        else if ( (strcmp(argv[ii],"-h" ) == 0 ) || (strcmp(argv[ii],"--help" )) )
+        {
+            printf("usage: PROGNAME \n\t-n number of floors [default 50]\n\t-p number of people [default 5]\n\t-m max capacity [default 2147483647]\n\t-t number of milliseconds per tick [default 1000]\n\t-w maximum amount of time a worker will be working on his task (in ticks) [default 50]\n\t-v [prints 100-tick aggregation data]\n\t-vv [prints only who enters and leaves each tick]\n\t-vvv [prints ALL the events that occur per tick] [default]\n\t-h or --help [prints this message]\n");
+            exit(0);
+        }
+        else if ( strcmp(argv[ii], "-p" ) == 0 )
+        {
+            if (ii == argc-1)
+            {
+                printf("please specify the number of people present");
+                exit (-1);
+            }
+            max_capacity = atoi(argv[++ii]);
+        } 
+        else if ( strcmp(argv[ii], "-n" ) == 0 )
+        {
+            if (ii == argc-1)
+            {
+                printf("please specify the number of floors in the building");
+                exit (-1);
+            }
+            floor_count = atoi(argv[++ii]);
+        } 
+        else if ( strcmp(argv[ii], "-m" ) == 0 )
+        {
+            if (ii == argc-1)
+            {
+                printf("please specify the maximum capacity of the elevator");
+                exit (-1);
+            }
+            max_capacity = atoi(argv[++ii]);
+        } 
+        else if ( strcmp(argv[ii], "-w" ) == 0 )
+        {
+            if (ii == argc-1)
+            {
+                printf("please specify the maximum amount of time a worker can work before changing floors");
+                exit (-1);
+            }
+            max_work_time = 1000*atoi(argv[++ii]);
+        } 
+        else if ( strcmp(argv[ii], "-t" ) == 0 )
+        {
+            if (ii == argc-1)
+            {
+                printf("please specify the number of miliseconds between ticks");
+                exit (-1);
+            }
+            tick_interval = 1000*atoi(argv[++ii]);
+        } 
+    } 
+    
+    requests = (int **)malloc(sizeof(int* ) * 2); //main() must allocate the memory - [2][floor_count]
+    requests[0] = (int *)malloc(sizeof(int)*floor_count);
+    requests[1] = (int *)malloc(sizeof(int)*floor_count);
+    targets = (int *) malloc(sizeof(int)*floor_count);   // main() must allocate the memory - [floor_count]
+    for (ii=0 ; ii < floor_count ; ii++)
+    {
+        requests[UP][ii]=0;
+        requests[DOWN][ii]=0;
+        targets[ii]=0;
+//        printf("executed: %d\n",ii);
+    }
+    
+    pthread_t elevator_thread, clock_thread, clients_threads[clients_num];
+    //init clock & elevator
+    pthread_create(&elevator_thread, NULL, elevator, (void *)max_capacity);
+    pthread_create(&clock_thread, NULL, my_clock, (void *)(tick_interval));
+
+    wait_for_tick();
+
+    //init  clients 
+    int rc;
+    for (ii = 0; ii < clients_num;ii++)
+    {
+        rc = pthread_create(&clients_threads[ii], NULL, &client, (void*)ii); //or is it just 'client'?
+        if (rc){
+            printf("ERROR; return code from pthread_create() is %d\n", rc);
+            exit(-1);
+        }
+    }
+    
+    pthread_exit(NULL);
+    return 0;
 }
